b633640db019d9ea450b9da9320a2e8da6436cea
SmurfNiZed <zipacaldo@up.edu.ph>
2023-12-16 01:17:49 +0800

    Feature: Displaying SOS in Map

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+
+import '../models/driver_emergency_model.dart';
 import '../services/mapbox.dart';
 import '../style/constants.dart';
 
@@ -12,26 +16,79 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  MapboxMap? mapboxMap;
+  late MapboxMapController _mapController;
+  late Timer _timer;
+  late StreamSubscription driverEmergencySubscription;
+  final CollectionReference driverEmergencyCollection = FirebaseFirestore.instance.collection('driver_emergency');
+  List<DriverEmergency> emergencies = [];
 
-  _onMapCreated(MapboxMap mapboxMap) {
-    this.mapboxMap = mapboxMap;
+  _onMapCreated(MapboxMapController controller) {
+    _mapController = controller;
+  }
+
+  void listenToEmergencies() {
+    driverEmergencySubscription = driverEmergencyCollection.snapshots().listen((QuerySnapshot snapshot) {
+      setState(() {
+        emergencies = snapshot.docs.map((doc) => DriverEmergency.fromSnapshot(doc)).toList();
+      });
+    });
+  }
+
+  void updateMapWithFilteredEmergencies() {
+    // Filter emergencies based on the timestamp
+    List<DriverEmergency> filteredEmergencies = emergencies
+        .where((emergency) =>
+    DateTime.now().difference(emergency.timestamp.toDate()).inSeconds < sosLifespan)
+        .toList();
+
+    // Update MapboxMap with the filtered emergencies
+    updateMap(filteredEmergencies);
+  }
+
+  void updateMap(List<DriverEmergency> emergencies) {
+    // Clear existing map markers
+    _mapController.clearCircles();
+
+    // Add red circles for each emergency
+    for (DriverEmergency emergency in emergencies) {
+      _mapController.addCircle(CircleOptions(
+        geometry: LatLng(emergency.location.latitude, emergency.location.longitude),
+        circleColor: '#F44336',
+        circleRadius: 5.0,
+        circleOpacity: 0.5
+      ));
+    }
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    listenToEmergencies();
+    _timer = Timer.periodic(const Duration(seconds: 5), (Timer t) =>
+    updateMapWithFilteredEmergencies());
+  }
+
+  @override
+  void dispose() {
+    driverEmergencySubscription.cancel();
+    _mapController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MapWidget(
-        resourceOptions: ResourceOptions(
-          accessToken: accessToken
+      body: MapboxMap(
+        accessToken: accessToken,
+        initialCameraPosition: const CameraPosition(
+            target: initialCameraPosition,
+            zoom: zoom
         ),
         onMapCreated: _onMapCreated,
-        styleUri: mapStyle,
-        cameraOptions: CameraOptions(
-          center: Point(coordinates: Position(14.655072, 121.068578)).toJson(),
-          padding: MbxEdgeInsets(top: Constants.defaultPadding, left: Constants.defaultPadding, bottom: Constants.defaultPadding, right: Constants.defaultPadding),
-          zoom: 20.0
-        ),
+        styleString: mapStyle,
+        rotateGesturesEnabled: false,
+        tiltGesturesEnabled: false,
       )
     );
   }
--- lib/services/mapbox.dart
+++ lib/services/mapbox.dart
@@ -1,2 +1,9 @@
+import 'package:mapbox_gl/mapbox_gl.dart';
+
 const accessToken = 'pk.eyJ1IjoiemVkZWMiLCJhIjoiY2xoZzdidjc1MDIxMDNsbnpocmloZXczeSJ9.qsTTfBC6ZB9ncP2rvbCnIw';
 const mapStyle = 'mapbox://styles/zedec/clhlz7j8z00kr01pp5she116z';
+const initialCameraPosition = LatLng(14.653836, 121.068427);
+const double zoom = 15.0;
+
+// in seconds
+const sosLifespan = 60;
